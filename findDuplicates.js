61ea71f6d25fce4348bdc87917336c6c5b4c88f1
Aaron Huynh <000473817@student.vcc.ca>
2024-08-16 18:06:48 -0700

    Find Duplicates solution in JS

---
+++ findDuplicates.js
@@ -0,0 +1,25 @@
+//sort array in ascending order
+//Find number closest to the left.
+//If the numder on the right side is the same, then smallset duplicate
+//since there are very few test cases, I can just brute force the sort using the built in sort method
+
+const input1 = [4, 10, 5, 1, 11, 5, 1 ,4 ,1];
+const input2 = [1, 10, 1, -1, 10, -1];
+
+let sorted1 = input1.sort();
+let sorted2 = input2.sort();
+
+console.log(sorted1);
+console.log(sorted2);
+
+function findDuplicates(arr) {
+    let duplicate = 0;
+    while (duplicate = 0) {
+        if (num(index) === num(index + 1))
+        {
+            return duplicate(num);
+        }
+    }
+}
+
+console.log (findDuplicates(sorted1));
